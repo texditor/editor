8ca582daffe5cbf3bee21c8266e7161386aec9bf
texditor <texditor@priveted.com>
2026-05-31 21:26:57 +0400

    Dev (#100)
    * fix: a bug in importing styles and file actions (#97)
    
    Co-authored-by: priveted <priveted@yandex.ru>
    
    * chore(release): update version to 2.0.2 (#99)
    
    ---------
    
    Co-authored-by: priveted <priveted@yandex.ru>

--- src/entities/blocks/files/actions/index.ts
+++ src/entities/blocks/files/actions/index.ts
@@ -1,7 +1,7 @@
-import MoveRightFileAction from "./MoveRightFileAction";
-import MoveLeftFileAction from "./MoveLeftFileAction";
-import EditFileAction from "./EditFileAction";
-import DownloadFileAction from "./DownloadFileAction";
-import DeleteFileAction from "./DeleteFileAction";
+import MoveRightFileAction from './MoveRightFileAction';
+import MoveLeftFileAction from './MoveLeftFileAction';
+import EditFileAction from './EditFileAction';
+import DownloadFileAction from './DownloadFileAction';
+import DeleteFileAction from './DeleteFileAction';
 
 export { MoveRightFileAction, MoveLeftFileAction, DownloadFileAction, DeleteFileAction, EditFileAction };
